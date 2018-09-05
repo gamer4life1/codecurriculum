903c06cc77b95ddcdf86344a030ee0b64c09130b
gamer4life1 <42851999+gamer4life1@users.noreply.github.com>
2018-09-05 10:29:47 -0700

    Update lesson1_variables.py

--- lesson1_variables.py
+++ lesson1_variables.py
@@ -1,5 +1,5 @@
 #This is a variable.
 bread = 156
-/*A variable can be assigned like this:
-  variable_name = value.
-  
+#A variable can be assigned like this:
+#variable_name = value.
+# A value can be a string, an integer
